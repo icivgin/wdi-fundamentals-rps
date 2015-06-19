d51d32679194b92bd7d420482e8e01e6c834f82a
Ian Civgin <iancivgin@Ians-MacBook-Pro.local>
2015-06-19 13:21:49 -0700

    Added functions playToFive() and playTo(x) as instructed. The rps game is now functional.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -74,8 +74,101 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var playerMove;
+    var computerMove;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    while (playerWins != 5 && computerWins != 5) {
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+		
+		if (getWinner(playerMove, computerMove) === 'player') {
+			playerWins += 1;
+			console.log('Player has won!');
+		}
+		else if (getWinner(playerMove, computerMove) === 'computer') {
+			computerWins += 1;
+			console.log('Coputer has won!');
+		}
+		else {
+		    console.log('It was a tie. You both chose "' + playerMove + '", play again!')
+		}
+	}
+	if (playerWins > computerWins) {
+		return 'Player wins, ' + playerWins + ' to ' + computerWins + '.';
+	}
+	else {
+		return 'Computer wins, ' + computerWins + ' to ' + playerWins + '.';
+	}
 }
 
+function playTo(x) {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var playerMove;
+    var computerMove;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    while (playerWins != x && computerWins != x) {
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+		
+		if (getWinner(playerMove, computerMove) === 'player') {
+			playerWins += 1;
+			console.log('Player has won!');
+		}
+		else if (getWinner(playerMove, computerMove) === 'computer') {
+			computerWins += 1;
+			console.log('Coputer has won!');
+		}
+		else {
+		    console.log('It was a tie. You both chose "' + playerMove + '", play again!')
+		}
+	}
+	if (playerWins > computerWins) {
+		return 'Player wins, ' + playerWins + ' to ' + computerWins + '.';
+	}
+	else {
+		return 'Computer wins, ' + computerWins + ' to ' + playerWins + '.';
+	}
+}
+
+//This function was a test to play a shorter game
+/*
+function playToTwo() {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var playerMove;
+    var computerMove;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    while (playerWins != 2 && computerWins != 2) {
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+		
+		if (getWinner(playerMove, computerMove) === 'player') {
+			playerWins += 1;
+			console.log('Player has won!');
+		}
+		else if (getWinner(playerMove, computerMove) === 'computer') {
+			computerWins += 1;
+			console.log('Coputer has won!');
+		}
+		else {
+		    console.log('It was a tie. You both chose "' + playerMove + '", play again!')
+		}
+	}
+	if (playerWins > computerWins) {
+		return 'Player wins, ' + playerWins + ' to ' + computerWins + '.';
+	}
+	else {
+		return 'Computer wins, ' + computerWins + ' to ' + playerWins + '.';
+	}
+}
+*/
+
+//This function was a test to play the game.
+/*
+function play() {
+	return 'The winner is ' + getWinner(getPlayerMove(), getComputerMove());
+}
+*/
